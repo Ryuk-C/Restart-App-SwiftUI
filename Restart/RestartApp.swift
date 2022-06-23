1d45d9db9bfb0bfba8a525ab7bd1451c6af695d1
Ryuk-C <cumahaznedar@gmail.com>
2022-06-24 00:03:57 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Cuma Haznedar on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
